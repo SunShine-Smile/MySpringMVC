94a1c316d3103d24a8a766895a62e0f46c7fe741
SunShine-Smile <328643720@qq.com>
2016-02-16 19:01:25 +0800

    1

---
+++ src/com/jason/springmvc/handlers/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.jason.springmvc.handlers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorld {
+
+	/**
+	 * 1、使用 @RequestMapping 注解来映射请求的URL
+	 * 2、返回值会通过视图解析器解析为实际的物理视图，对于InternalResourceViewResolver 试图解析器，会做如下解析
+	 * 通过 prefix + returnVal + suffix 这样的方式得到实际的物理视图，然后做转发
+	 * 
+	 * /WEB-INF/views/success.jsp 
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/helloworld")
+	public String hello(){
+		System.out.println("Hello World");
+		return "success";
+	}
+	
+}
